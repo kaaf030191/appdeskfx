c808c30c17bce6e81db9a610e8d301fe57e1584d
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2025-01-18 13:00:22 -0500

    Init: Inicialización.
    Inicialización general.

---
+++ src/main/java/com/codideep/app/Main.java
@@ -0,0 +1,15 @@
+package com.codideep.app;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+/**
+ *
+ * @author KAAF0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello!!!");
+    }
+}
